6792a26d90e6a0b259955ee4af74bedd4319554e
teor <teor2345@gmail.com>
2017-07-16 22:00:29 +1000

    Tidy up some of the old scripts

--- client-or-circuit-link.py
+++ client-or-circuit-link.py
@@ -3,15 +3,11 @@
 # Tested: Python 2.7.13 on macOS 10.12.5 with OpenSSL 1.0.2l and tor 0.3.0.9.
 # (The default OpenSSL on macOS is *very* old.)
 
-import binascii
-
 from endosome import *
 
 # The default IP and Port
 RELAYIP = '127.0.0.1'
 ORPORT = 12345
-
-MAX_RESPONSE_LEN = 10*1024*1024
 
 # Request:
 # VERSIONS, NETINFO, CREATE_FAST
@@ -33,12 +29,12 @@ cell_list.append(create_fast_cell)
 # Try the default set of link versions: we will get 4 or 5, depending on
 # the Tor version
 print 'SSL Server: {}:{}'.format(RELAYIP, ORPORT)
-print 'Opening Tor connection, sending link versions cell only'
+print 'Opening Tor connection, sending link version and netinfo cells'
 (context, response_bytes) = link_request_cell_list(RELAYIP, ORPORT,
                                                    cell_list)
 print 'Connection context:\n{}'.format(link_format_context(context))
 print 'Connection cells:\n{}'.format(link_format_cell_bytes(context,
-                                                           response_bytes))
+                                                            response_bytes))
 
 # Try link version 3
 print 'SSL Server: {}:{}'.format(RELAYIP, ORPORT)
@@ -48,4 +44,4 @@ print 'Opening Tor connection, sending link versions cell only'
                                                    link_version_list=[3])
 print 'Connection context:\n{}'.format(link_format_context(context))
 print 'Connection cells:\n{}'.format(link_format_cell_bytes(context,
-                                                           response_bytes))
+                                                            response_bytes))
--- client-or-link.py
+++ client-or-link.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
-# Do a Tor OR client version negotiation with a local Tor relay
+# Do a Tor OR link version negotiation with a local Tor relay
 # Tested: Python 2.7.13 on macOS 10.12.5 with OpenSSL 1.0.2l and tor 0.3.0.9.
 # (The default OpenSSL on macOS is *very* old.)
-
-# This script just sends a versions cell
-
-import binascii
 
 from endosome import *
 
@@ -14,22 +10,10 @@ RELAYIP = '127.0.0.1'
 ORPORT = 12345
 
 # Request:
-# VERSIONS: CircID(2)=None CommandCode=VERSIONS PayloadLength=2
-#           SupportedVersionList=3,4,5
+# VERSIONS, NETINFO
+#
 # Expected Response:
-# VERSIONS: CircID(2)=None CommandCode=VERSIONS PayloadLength=4
-#           SupportedVersionList=3,4
-# CERTS: CircID(4)=None CommandCode=CERTS N=5 (CertType CLEN Certificate)*5
-#   https://gitweb.torproject.org/torspec.git/tree/tor-spec.txt#n547
-#   ed25519: https://trac.torproject.org/projects/tor/ticket/22861
-# AUTH_CHALLENGE: CircID(4)=None CommandCode=AUTH_CHALLENGE
-#                 Challenge(32)=RandomBytes N_Methods(2)=2 Methods=1,3
-#   https://gitweb.torproject.org/torspec.git/tree/tor-spec.txt#n604
-#   ed25519: https://trac.torproject.org/projects/tor/ticket/22861
-# NETINFO: CircID(4)=None CommandCode=NETINFO Timestamp=Now
-#          RemoteAddressType=IPv4 RemoteAddressLength=4
-#          RemoteAddress=127.0.0.1 LocalAddressCount=1 LocalAddressType=IPv4
-#          LocalAddressLength=4 LocalAddress=RelayPublicIPAddress ZeroPad(491)
+# VERSIONS, CERTS, AUTH_CHALLENGE, NETINFO
 
 # Try the default set of link versions: we will get 4 or 5, depending on
 # the Tor version
@@ -51,3 +35,12 @@ print 'Connection context:\n{}'.format(link_format_context(context))
 print 'Connection cells:\n{}'.format(link_format_cell_bytes(context,
                                                            response_bytes))
 
+# Try without netinfo
+print 'SSL Server: {}:{}'.format(RELAYIP, ORPORT)
+print 'Opening Tor connection, sending link versions cell only'
+(context, response_bytes) = link_request_cell_list(RELAYIP, ORPORT,
+                                                   [],
+                                                   send_netinfo=False)
+print 'Connection context:\n{}'.format(link_format_context(context))
+print 'Connection cells:\n{}'.format(link_format_cell_bytes(context,
+                                                            response_bytes))
